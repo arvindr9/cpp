de24d9444eefee71a07fb902df686fb5d32a42a1
Arvind Ramaswami <saramaswami@gmail.com>
2017-11-12 22:59:06 -0500

    Add files via upload

---
+++ rainbow.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void dfs(int u, int rainbowDown[], vector<pair<int,int>> adj[]) {
+    for(auto v: adj[u]) {
+        if(!rainbowDown[v.first]) {
+            rainbowDown[u] = 0;
+            break;
+        }
+        dfs(v, rainbowDown, *adj);
+        for(auto w: v.first) {
+            if(v.second == w.second) {
+                rainbowDown[u] = 0;
+                break;
+            }
+            rainbowDown[u] = 0;
+        } 
+    }
+    rainbowDown[u] = 1;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    int N;
+    cin >> N;
+    int a, b, c;
+    vector<pair<int, int>> adj[N+1];
+    while (cin >> a >> b >> c) {
+        adj[a].push_back(make_pair(b, c));
+        adj[b].push_back(make_pair(a, c));
+    }
+    int rainbowDown[N+1] = {0};
+    int rainbowUp[N+1] = {0};
+    for(int i = 1; i <= N; i++) {
+        int count = 0;
+        for (auto x: adj[i]) {
+            count++;
+        }
+        if(count == 1) rainbowDown[i] = 1;
+    }
+    int root = 1;
+    
+}
